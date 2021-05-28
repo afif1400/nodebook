49c675246767f5b7a2166f302791a8d0fc52d482
afif ahmed <afifahmed456123@gmail.com>
2021-05-28 19:10:22 +0530

    inital setup and plugins building initation

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from 'react';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+	const ref = useRef<any>();
+	const [input, setInput] = useState('');
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	const [code, setCode] = useState('');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+	const startService = async () => {
+		ref.current = await esbuild.startService({
+			worker: true,
+			wasmURL: '/esbuild.wasm',
+		});
+	};
+
+	useEffect(() => {
+		startService();
+	}, []);
+
+	const onClick = async () => {
+		if (!ref.current) {
+			return;
+		}
+
+		const result = await ref.current.transform(input, {
+			loader: 'jsx',
+			target: 'es2015',
+		});
+
+		setCode(result.code);
+	};
+
+	return (
+		<div>
+			<textarea
+				onChange={(e) => setInput(e.target.value)}
+				value={input}
+			></textarea>
+			<div>
+				<button
+					style={{
+						background: '#222',
+						color: '#fff',
+						outline: 'none',
+						border: 'none',
+						padding: '5px 10px',
+						margin: '10px 0',
+						borderRadius: '5px',
+					}}
+					onClick={onClick}
+				>
+					Submit
+				</button>
+			</div>
+			<pre>{code}</pre>
+		</div>
+	);
+};
+
+ReactDom.render(<App />, document.querySelector('#root'));
