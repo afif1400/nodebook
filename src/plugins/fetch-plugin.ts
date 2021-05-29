661b31d608332e9783133fce598e2a74320610c2
afif ahmed <afifahmed456123@gmail.com>
2021-05-29 20:12:19 +0530

    added support for tranpiling css imports

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -6,41 +6,69 @@ const fileCache = localForage.createInstance({
 	name: 'filecache',
 });
 
+//? this onLoad function overrides the default esbuild's onLoad  which is
+//? used to fetch modules from unpkg.com
+
 export const fetchPlugin = (inputCode: string) => {
 	return {
 		name: 'fetch-plugin',
 		setup(build: esbuild.PluginBuild) {
-			//? this below function overrides the default esbuild's onLoad  which is
-			//? used to fetch modules from unpkg.com
+			build.onLoad({ filter: /(^index\.js$)/ }, (): esbuild.OnLoadResult => {
+				return {
+					loader: 'jsx',
+					contents: inputCode,
+				};
+			});
+
+			build.onLoad({ filter: /.*/ }, async (args: any) => {
+				//* Check to see if we have already fetched a file before
+				//* and is present in the cache
+				const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+					args.path
+				);
+
+				//* If present, return it immediately
+				if (cachedResult) {
+					return cachedResult;
+				}
+			});
+
+			//? onLoad for CSS files
+			build.onLoad(
+				{ filter: /.css$/ },
+				async (args: any): Promise<esbuild.OnLoadResult> => {
+					const { data, request } = await axios.get(args.path);
+
+					const escapedCSS = data
+						.replace(/\n/g, '')
+						.replace(/"/g, '\\"')
+						.replace(/'/g, "\\'");
+					const contents = `
+          const style = document.createElement('style');
+          style.innerText = '${escapedCSS}';
+          document.head.appendChild(style)
+          `;
+
+					const result: esbuild.OnLoadResult = {
+						loader: 'jsx',
+						contents,
+						resolveDir: new URL('./', request.responseURL).pathname,
+					};
+					//* store response in the cache
+					await fileCache.setItem(args.path, result);
+
+					return result;
+				}
+			);
+
+			//? onLoad for javascript files
 			build.onLoad(
 				{ filter: /.*/ },
 				async (args: any): Promise<esbuild.OnLoadResult> => {
-					console.log('onLoad', args);
-
-					if (args.path === 'index.js') {
-						return {
-							loader: 'jsx',
-							contents: inputCode,
-						};
-					}
-
-					//* Check to see if we have already fetched a file before
-					//* and is present in the cache
-					const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
-						args.path
-					);
-
-					//* If present, return it immediately
-					if (cachedResult) {
-						return cachedResult;
-					}
-
 					const { data, request } = await axios.get(args.path);
 
-					const loader = args.path.match(/.css$/) ? 'css' : 'jsx';
-
 					const result: esbuild.OnLoadResult = {
-						loader,
+						loader: 'jsx',
 						contents: data,
 						resolveDir: new URL('./', request.responseURL).pathname,
 					};
