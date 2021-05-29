e2da546dfcdbd4be522756b25caad2dab834ca80
afif ahmed <afifahmed456123@gmail.com>
2021-05-30 01:39:15 +0530

    monaco editor additional configuration

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -4,7 +4,21 @@ const CodeEditor = () => {
 	return (
 		<div>
 			<h1>Code Editor</h1>
-			<MonacoEditor language='javascript' height='500px ' />
+			<MonacoEditor
+				language='javascript'
+				theme='vs-dark'
+				height='500px'
+				options={{
+					wordWrap: 'on',
+					minimap: { enabled: false },
+					showUnused: false,
+					folding: false,
+					lineNumbersMinChars: 3,
+					fontSize: 18,
+					scrollBeyondLastLine: false,
+					automaticLayout: true,
+				}}
+			/>
 		</div>
 	);
 };
